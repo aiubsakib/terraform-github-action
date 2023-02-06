30c8648812a4f8504d17657b6e434bafc8e7a938
sabbir <aiubsakib@gmail.com>
2023-02-06 10:17:02 +0100

    push

--- main.tf
+++ main.tf
@@ -1,21 +1,6 @@
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.7.0"
-    }
-  }
-
-required_version = ">=1.2.0"
+resource "azurerm_resource_group" "rg" {
+  name     = var.rgname
+  location = var.location
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
-
-data "azurerm_resource_group" "ResGroup" {
-  name = "RG-SCUSTFStorage"
-  location = "East US"
-}
--- output.tf
+++ output.tf
@@ -1,3 +1,5 @@
-output "Storage_Account_Name" {
-  value = azurerm_storage_account.StorAccount1.name
+output "resourcegroup_name" {
+  value = {
+      rg_name= azurerm_resource_group.rg.name
+  } 
 }
